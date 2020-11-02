ad8e5bdf8d615f70f653d0194af22e3e62ecdfae
Alex Kenan <alexkenan@users.noreply.github.com>
2020-11-02 17:17:41 -0500

    Update chap8.py

--- chapter 8/chap8.py
+++ chapter 8/chap8.py
@@ -2,7 +2,7 @@
 """
 Graphically show camera shutter effect
 
-Produced for "Python for Mechanical and Aerospace Engineering" by Alex Kenan, ISBN 978-1-7360606-0-5.
+Produced for "Python for Mechanical and Aerospace Engineering" by Alex Kenan, ISBN 978-1-7360606-0-5 and 978-1-7360606-1-2.
 Copyright © 2020 Alexander Kenan. Some Rights Reserved. This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 See http://creativecommons.org/licenses/by-nc-sa/4.0/ for more information.
 """
