c521d3615be258bb072564202023c18c4a54454f
Alex Kenan <alexkenan@users.noreply.github.com>
2020-11-02 17:19:52 -0500

    Update chap4.py

--- chapter 4/chap4.py
+++ chapter 4/chap4.py
@@ -1,6 +1,10 @@
 # !/usr/bin/env python3
 """
 Graph 2D airfoils using University of Indiana at Urbana-Champaign (UIUC) website
+
+Produced for "Python for Mechanical and Aerospace Engineering" by Alex Kenan, ISBN 978-1-7360606-0-5 and 978-1-7360606-1-2.
+Copyright © 2020 Alexander Kenan. Some Rights Reserved. This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
+See http://creativecommons.org/licenses/by-nc-sa/4.0/ for more information.
 """
 import requests
 import matplotlib.pyplot as plt
