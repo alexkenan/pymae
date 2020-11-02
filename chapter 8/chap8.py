2c050334db979b7dec9192be01570ab08e0b6610
Alex Kenan <alexkenan@users.noreply.github.com>
2020-11-02 17:07:58 -0500

    Add files via upload

---
+++ chapter 8/chap8.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Graphically show camera shutter effect
+"""
+
+import tkinter as tk
+import numpy as np
+from PIL import Image, ImageTk
+
+root = tk.Tk()
+stage = tk.Label(root)
+stage.pack()
+height = 300
+num_propeller_blades = 4
+x, y = np.ogrid[-height/2: height/2, -height/2: height/2]
+plane = x - 1j * y
+bentprop = np.zeros_like(plane, dtype=np.bool)
+
+for frame in range(height):
+    backgnd = np.zeros_like(plane, dtype=np.uint8)
+    propeller = np.zeros_like(plane, dtype=np.bool)
+    angle = 2 * np.pi * (frame / height)
+    for blade in range(num_propeller_blades):
+        phase = np.exp(1j * (angle + blade * np.pi / (num_propeller_blades / 2)))
+        ellipse = abs(plane - 0.48 * height * phase) + abs(plane)
+        propeller |= ellipse < 0.49 * height
+
+    bentprop[frame] = propeller[frame]
+    shutter_pan = [f for f in range(frame, min(frame + 3, height - 3))]
+    colors = ("white", "black", "red", "blue")
+    rgbcolors = np.array(list(map(root.winfo_rgb, colors))) / 256
+    composite = np.maximum.reduce((backgnd, propeller * 1, bentprop * 2))
+    composite[shutter_pan] = 3
+    rgb = rgbcolors.astype(np.uint8)[composite]
+    image = ImageTk.PhotoImage(Image.fromarray(rgb, mode="RGB"))
+    stage.config(image=image)
+    root.update()
+
+root.lift()
+root.attributes('-topmost', True)
+root.after_idle(root.attributes, '-topmost', False)
+root.title('{} blade propeller camera effect'.format(num_propeller_blades))
+root.mainloop()
