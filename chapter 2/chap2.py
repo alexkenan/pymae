58eea83aacf4a9eb483736f02fc52ddc09f8de61
Alex Kenan <alexkenan@users.noreply.github.com>
2020-11-02 17:20:24 -0500

    Update chap2.py

--- chapter 2/chap2.py
+++ chapter 2/chap2.py
@@ -1,6 +1,10 @@
 # !/usr/bin/env python3
 """
 Graph thrust required and thrust available at sea level, 10,000 ft, and 35,000 ft (fl350)
+
+Produced for "Python for Mechanical and Aerospace Engineering" by Alex Kenan, ISBN 978-1-7360606-0-5 and 978-1-7360606-1-2.
+Copyright © 2020 Alexander Kenan. Some Rights Reserved. This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
+See http://creativecommons.org/licenses/by-nc-sa/4.0/ for more information.
 """
 
 from numpy import pi
